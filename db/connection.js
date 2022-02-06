56349a189db3a5bf7a60521f45afaabe9e264a76
Luke <lukeconrad.stew@gmail.com>
2022-02-05 21:17:06 -0800

    question list prompt

--- db/connection.js
+++ db/connection.js
@@ -1,22 +1,25 @@
-const mysql2 = require('mysql2');
+const mysql = require('mysql2');
+const util = require('util');
 
 // Connect to database
 const connection = mysql.createConnection(
-    {
-      host: 'localhost',
-      // MySQL username,
-      user: 'root',
-      // MySQL password
-      password: 'password',
-      database: 'employees_db'
-    });
+  {
+    host: 'localhost',
+    // MySQL username,
+    user: 'root',
+    // MySQL password
+    password: 'password',
+    database: 'employees_db'
+  });
 
-    connection.connect(function (err) {
-        if (err) {
-          throw err;
-        } else {
-            console.log("Sucessfully connected to mysql")
-        } 
-      });
-      
-      module.exports = connect
+connection.query = util.promisify(connection.query);
+
+connection.connect(function (err) {
+  if (err) {
+    throw err;
+  } else {
+    console.log("Sucessfully connected to mysql")
+  }
+});
+
+module.exports = connection; 
