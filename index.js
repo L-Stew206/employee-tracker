bc2cd5fdfaff20796c3a80df262c28963131f813
Luke <lukeconrad.stew@gmail.com>
2022-02-17 16:05:44 -0800

    update employee test

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ const startQuestion = () => {
             },
         ])
         .then(({ response }) => {
+             console.log(response)
             if (response == 'View all Departments') {
                 console.log("welcome");
                 viewAllDepartments();
@@ -37,7 +38,8 @@ const startQuestion = () => {
                 addRole();
             } else if (response == 'Add an Employee') {
                 addEmployee();
-            } else if (response == 'Update an Employee role') {
+            } else if (response == 'Update a Employee role') {
+                console.log("work please")
                 updateEmployee();
             };
         });
@@ -92,6 +94,7 @@ function addDepartment() {
                 message: "Enter the name of the new department:",
             },
         ])
+        // Adds new information to departments table
         .then((response) => {
             const query = `INSERT INTO departments (department_name) VALUES (?)`;
             db.query(query, response.department, (err, results) => {
@@ -107,21 +110,17 @@ function addDepartment() {
 // add a role - CREATE - "INSERT [table_name] (col1, col2) VALUES (value, value2)""
 async function addRole() {
 
+    // Selects all departments
     let departments = await db.query('SELECT * FROM departments;');
 
-   // let departmentsArray = {};
-
-    // departments.forEach(department => {
-    //     departmentsArray[department.department_name] = department.id;
-    // });
-
+    // Creates an array of departments
     let departmentID = departments.map(department => {
         return {
             name: department.department_name,
             value: department.id
         }
     });
-    
+
     inquirer
         .prompt([
             {
@@ -141,114 +140,160 @@ async function addRole() {
                 choices: departmentID
             },
         ])
-        .then (async (response) => {
+        .then(async (response) => {
+
             // Take the user's answers and go INSERT them into the 'role' table
-            db.query ('INSERT INTO roles (job_title, salary, department_id) VALUES (?,?,?)',
-            [response.role,response.salery,response.department_name],
-            
-           (err, results) => {
-                if (err) {
-                  console.log(err);
-                } else {
-                  startQuestion();
+            db.query('INSERT INTO roles (job_title, salary, department_id) VALUES (?,?,?)',
+                [response.role, response.salery, response.department_name],
+
+                (err, results) => {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        startQuestion();
+                    }
                 }
-              }
             )
         });
-    }
-            
-        
-    
-
-    // SELECT the existing department put for the 'department' table
-
-
+}
 
 // add an employee - CREATE - "INSERT" [table_name]
-async function addEmployee (){
+async function addEmployee() {
 
     try {
+        //  Selects all roles
+        let roles = await db.query('SELECT * FROM roles;');
 
-    
-    let roles = await db.query('SELECT * FROM roles;');
+        // Creats an array of roles to choose from
+        let roleID = roles.map(role => {
+            return {
+                name: role.job_title,
+                value: role.id
+            }
+        });
 
+
+        // Selects all employees
+        let employees = await db.query('SELECT * FROM employee;');
+
+        // Creates an array if employees to choose from 
+        let employeeID = employees.map(employee => {
+            return {
+                name: employee.first_name + " " + employee.last_name,
+
+                value: employee.id
+            }
+        });
+
+        inquirer
+            .prompt([
+                {
+                    name: "firstname",
+                    type: "input",
+                    message: "Enter the employee's first name:",
+                },
+                {
+                    name: "lastname",
+                    type: "input",
+                    message: "Enter the employee's last name:",
+                },
+                {
+                    name: "role",
+                    type: "list",
+                    message: "Choose a role for this employee",
+                    choices: roleID,
+                },
+                {
+                    name: "manager",
+                    type: "list",
+                    message: "Who is the employee's manager?",
+                    choices: employeeID,
+                },
+            ])
+            .then(async (response) => {
+                console.log(response);
+                db.query('INSERT INTO employee (first_name,last_name,role_id,manager_id) VALUES (?,?,?,?)',
+                    [response.firstname, response.lastname, response.role, response.manager],
+                    (err, results) => {
+                        if (err) {
+                            console.log(err);
+                        } else {
+                            startQuestion();
+                        }
+                    }
+                )
+            });
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+
+//update an employee 
+async function updateEmployee() {
+    console.log("helo")
+    try {
+        console.log("helo");
+
+    // Selects all employees
+    let employees = await db.query('SELECT first_name, last_name FROM employee;');
+
+    // Creates an array if employees to choose from 
+    let employeeID = employees.map(employee => {
+        return {
+            name: employee.first_name + " " + employee.last_name,
+
+            value: employee.id
+        }
+        console.log("helo");
+    });
+
+    //  Selects all roles
+    let roles = await db.query('SELECT job_title, department_id FROM roles;');
+
+    // Creats an array of roles to choose from
     let roleID = roles.map(role => {
         return {
             name: role.job_title,
             value: role.id
         }
-         });
-    let employees = await db.query('SELECT * FROM employee;');
-
-    let employeeID = employees.map(employee => {
-        return {
-            name: employee.first_name + " " + employee.last_name,
-            
-            value: employee.id
-        }  
     });
 
     inquirer
-    .prompt([
-        {
-            name: "firstname",
-            type: "input",
-            message: "Enter the employee's first name:",
-          },
-          {
-            name: "lastname",
-            type: "input",
-            message: "Enter the employee's last name:",
-          },
-          {
-            name: "role",
-            type: "list",
-            message: "Choose a role for this employee",
-            choices: roleID,
-          },
-          {
-            name: "manager",
-            type: "list",
-            message: "Who is the employee's manager?",
-            choices: employeeID,
-          },
+        .prompt([
+            {
+                name: "employee_firstName",
+                type: "list",
+                message: "Select the employee you want to update",
+                choices: employeeID,
+            },
+            {
+                name: "new_role",
+                type: "list",
+                message: "Choose the employee's new role",
+                choices: roleID
+            },
         ])
-        .then (async (response) => {
-            console.log(response);
-            db.query( 'INSERT INTO employee (first_name,last_name,role_id,manager_id) VALUES (?,?,?,?)',
-            [response.firstname,response.lastname,response.role,response.manager],
-            (err, results) => {
-                if (err) {
-                  console.log(err);
-                } else {
-                  startQuestion();
+
+    
+
+
+
+        .then(async (response) => {
+            var firstName = response.employee_firstName.split([1]);
+            db.query('UPDATE employee (first_name,last_name,role_id) VALUES (?,?,?)',
+                [response.employee_firstName, response.employee_lastName, response.new_role],
+                (err, results) => {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        startQuestion();
+                    }
                 }
-              }
             )
-          });
-        } catch (err) {
-            console.log(err)
-        }
-      }
-     
-
-//update an employee 
-async function updateEmployee(){
-    inquirer
-    .prompt ([
-        {
-            name: "employee_name",
-            type: "list",
-            message: "  Select the employee you want to update",
-            choices: employeeID
-          },
-          {
-            name: "job_title",
-            type: "list",
-            message: "Enter the employee's last name:",
-            choices: rolesID
-          },
-
-        
-    ])
+        });
+    } catch (err) {
+    console.log(err)
+    }   
 }
+    
